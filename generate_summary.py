0bbf5bca88e1f3b3dd31afe713150a05b444aa67
abrie <abrie@users.noreply.github.com>
2024-11-18 20:28:48 -0500

    Update GraphQL query and fetch_issues function to include commit messages
    * Update the GraphQL query to fetch commit messages for each PR using `commits(first: 100) { nodes { commit { messageHeadline } } }`
    * Update the `fetch_issues` function to include commit messages in the PR data by extracting `messageHeadline` from the commit nodes

--- generate_summary.py
+++ generate_summary.py
@@ -22,7 +22,9 @@ def fetch_issues():
             merged
             commits(first: 100) {
               nodes {
-                message
+                commit {
+                  messageHeadline
+                }
               }
             }
           }
@@ -41,7 +43,7 @@ def fetch_issues():
     for pr in pull_requests:
         pr['is_pr'] = True
         pr['merged'] = pr.get('merged', False)
-        pr['commits'] = [commit['message'] for commit in pr['commits']['nodes']]
+        pr['commits'] = [commit['commit']['messageHeadline'] for commit in pr['commits']['nodes']]
     combined_list = issues + pull_requests
     combined_list.sort(key=lambda x: x['createdAt'])
     return combined_list
